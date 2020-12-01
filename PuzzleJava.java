5c3c1dde52413baa90561da1b2899abcce2dded4
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-01 14:29:02 -0800

    Fixed methods in PuzzleJava class and PuzzleJavaTest class

--- PuzzleJava.java
+++ PuzzleJava.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -6,31 +7,32 @@ import java.util.Random;
 public class PuzzleJava {
     
         // PUZZLE 01
-        // ● Create an array with the following values: 3,5,1,2,7,9,8,13,25,32. 
+        //  Create an array with the following values: 3,5,1,2,7,9,8,13,25,32. 
         // Print the sum of all numbers in the array. Also have the function return an array that only 
         // includes numbers that are greater than 10 (e.g. when you pass the array above, 
         // it should return an array with the values of 13,25,32)
-        public ArrayList<Integer> Puzzele01(ArrayList<Integer> arr) {
+        public ArrayList<Integer> PuzzeleI(ArrayList<Integer> arr) {
             int sum = 0;
-            ArrayList<Integer> newarr = new ArrayList<Integer>();
+            System.out.print("Old array: " + arr + "\n");
+            ArrayList<Integer> newArr = new ArrayList<Integer>();
             for (int i = 0; i < arr.size(); i++) {
                 sum = sum + arr.get(i);
-                if (i > 10) {
-                    newarr.add(i);
+                if (arr.get(i) > 10) {
+                    int x = arr.get(i);
+                    newArr.add(x);
                 }
             }
-            System.out.print(sum);
-            return newarr;
+            System.out.println("Sum: " + sum);
+            return newArr;
         }
 
 
         // PUZZLE 02
-        // ● Create an array with the following values: Nancy, Jinichi, Fujibayashi, Momochi, Ishikawa. 
+        //  Create an array with the following values: Nancy, Jinichi, Fujibayashi, Momochi, Ishikawa. 
         // Shuffle the array and print the name of each person. Have the method also
         // return an array with names that are longer than 5 characters.
-        public ArrayList<String> Puzzele02(ArrayList<String> arr) {
+        public ArrayList<String> PuzzeleII(ArrayList<String> arr) {
             for (int i = 0; i < arr.size(); i++) {
-                System.out.print(arr.get(i));
                 if (arr.get(i).length() < 6) {
                     arr.remove(i);
                 }
@@ -40,28 +42,36 @@ public class PuzzleJava {
 
 
         // PUZZLE 03
-        // ● Create an array that contains all 26 letters of the alphabet (this array must have 26 values). 
+        //  Create an array that contains all 26 letters of the alphabet (this array must have 26 values). 
         // Shuffle the array and, after shuffling, display the last letter from the array. 
         // Have it also display the first letter of the array. 
         // If the first letter in the array is a vowel, have it display a message.
-        public ArrayList<Character> Puzzele03(ArrayList<Character> arr) {
-            Collections.shuffle(arr);
-            System.out.print(arr.get(arr.size() - 1));
-            System.out.print(arr.get(0));
-            if (arr.get(0) == 'a' || arr.get(0) == 'e' || arr.get(0) == 'i' || arr.get(0) == 'o' || arr.get(0) == 'u'){
-                System.out.print(arr.get(0) + " Is a Vowel");
+        public ArrayList<Character> PuzzeleIII() {
+            String abc = "abcdefghijklmnopqrstuvwxyz";
+            ArrayList<Character> letters = new ArrayList<Character>();
+            for (int i = 0; i < abc.length(); i++) {
+                char let = abc.charAt(i);
+                letters.add(let);
             }
-            return arr;
+            System.out.println("Alphabet List: " + letters);
+            System.out.println("Shuffling List!");
+            Collections.shuffle(letters);
+            System.out.println("Last Letter: " + letters.get(letters.size() - 1));
+            System.out.println("First Letter: " + letters.get(0));
+            if (letters.get(0) == 'a' || letters.get(0) == 'e' || letters.get(0) == 'i' || letters.get(0) == 'o' || letters.get(0) == 'u'){
+                System.out.println(letters.get(0) + " Is a Vowel");
+            }
+            return letters;
         }
 
 
         // PUZZLE 04
-        // ● Generate and return an array with 10 random numbers between 55-100.
-        public ArrayList<Integer> Puzzele04() {
+        //  Generate and return an array with 10 random numbers between 55-100.
+        public ArrayList<Integer> PuzzeleIV() {
             ArrayList<Integer> arr = new ArrayList<Integer>();
             for (int i = 0; i < 10; i++) {
                 Random x = new Random();
-                int num = x.nextInt(100 - 55 + 1);
+                int num = x.nextInt(100 - 55) + 55;
                 arr.add(num);
             }
             return arr;
@@ -69,19 +79,19 @@ public class PuzzleJava {
 
 
         // PUZZLE 05
-        // ● Generate and return an array with 10 random numbers between 55-100 and 
+        //  Generate and return an array with 10 random numbers between 55-100 and 
         // have it be sorted (showing the smallest number in the beginning). 
         // Display all the numbers in the array. Next, display the minimum value in the array 
         // as well as the maximum value.
-        public ArrayList<Object> Puzzele05() {
-            ArrayList<Object> arr = new ArrayList<Object>();
+        public ArrayList<Integer> PuzzeleV() {
+            ArrayList<Integer> arr = new ArrayList<Integer>();
             for (int i = 0; i < 10; i++) {
                 Random x = new Random();
-                int num = x.nextInt(100 - 55 +1);
+                int num = x.nextInt(100 - 55) + 55;
                 arr.add(num);
             }
-            Object max = arr.get(0);
-            Object min = arr.get(0);
+            int max = arr.get(0);
+            int min = arr.get(0);
             for (int i = 0; i < 10; i++) {
                 if ((int) arr.get(i) > (int) max) {
                     max = arr.get(i);
@@ -90,8 +100,9 @@ public class PuzzleJava {
                     min = arr.get(i);
                 }
             }
-            String mx = "Max = " + max;
-            String mn = "Min = " + min;
+            Collections.sort(arr);
+            int mx = max;
+            int mn = min;
             arr.add(mx);
             arr.add(mn);
             return arr;
@@ -99,10 +110,10 @@ public class PuzzleJava {
 
 
         // PUZZLE 06
-        // ● Create a random string that is 5 characters long.
-        public String Puzzele06() {
-            String abc = "abcdefghijklmnopqrstuvwxyz"
-            String str;
+        //  Create a random string that is 5 characters long.
+        public String PuzzeleVI() {
+            String abc = "abcdefghijklmnopqrstuvwxyz";
+            String str = new String();
             for (int i = 0; i < 5; i++) {
                 int x = new Random().nextInt(26);
                 String letter = String.valueOf(abc.charAt(x));
@@ -113,12 +124,12 @@ public class PuzzleJava {
 
 
         // PUZZLE 07
-        // ● Generate an array with 10 random strings that are each 5 characters long
-        public ArrayList<String> Puzzele07() {
-            String abc = "abcdefghijklmnopqrstuvwxyz"
-            ArrayList<String> strArr;
-            for (int i = 0; i < 10; i++) {
-                String str;
+        //  Generate an array with 10 random strings that are each 5 characters long
+        public ArrayList<String> PuzzeleVII() {
+            String abc = "abcdefghijklmnopqrstuvwxyz";
+            ArrayList<String> strArr = new ArrayList<String>();
+            for (int j = 0; j < 10; j++) {
+                String str = new String();
                 for (int i = 0; i < 5; i++) {
                     int x = new Random().nextInt(26);
                     String letter = String.valueOf(abc.charAt(x));
--- PuzzleJavaTest.java
+++ PuzzleJavaTest.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class PuzzleJavaTest {
@@ -8,58 +9,65 @@ public class PuzzleJavaTest {
         PuzzleJava uzz = new PuzzleJava();
         
         // PUZZLE 01
-        // ● Create an array with the following values: 3,5,1,2,7,9,8,13,25,32. 
+        System.out.println("PUZZLE 01");
+        //  Create an array with the following values: 3,5,1,2,7,9,8,13,25,32. 
         // Print the sum of all numbers in the array. Also have the function return an array that only 
         // includes numbers that are greater than 10 (e.g. when you pass the array above, 
         // it should return an array with the values of 13,25,32)
-        ArrayList<Integer> values = new ArrayList<Integer>(Arrays.asList(3,5,1,2,7,9,8,13,25,32));
-        ArrayList<Integer> changed = uzz.Puzzle01(values);
-        System.out.println(changed);
+        ArrayList<Integer> values = new ArrayList<Integer>( Arrays.asList( 3,5,1,2,7,9,8,13,25,32 ) );
+        ArrayList<Integer> changed = uzz.PuzzeleI(values);
+        System.out.println("New array: " + changed);
 
 
         // PUZZLE 02
-        // ● Create an array with the following values: Nancy, Jinichi, Fujibayashi, Momochi, Ishikawa. 
+        System.out.println("PUZZLE 02");
+        //  Create an array with the following values: Nancy, Jinichi, Fujibayashi, Momochi, Ishikawa. 
         // Shuffle the array and print the name of each person. Have the method also
         //  return an array with names that are longer than 5 characters.
         ArrayList<String> nameList = new ArrayList<String>(Arrays.asList("Nancy", "Jinichi", "Fujibayashi", "Momochi", "Ishikawa"));
-        ArrayList<String> bigNames = uzz.Puzzle02(nameList);
-        System.out.println(bigNames);
+        System.out.println("List Names: " + nameList);
+        ArrayList<String> bigNames = uzz.PuzzeleII(nameList);
+        System.out.println("Names greater than 5 letters: " + bigNames);
 
 
         // PUZZLE 03
-        // ● Create an array that contains all 26 letters of the alphabet (this array must have 26 values). 
+        System.out.println("PUZZLE 03");
+        //  Create an array that contains all 26 letters of the alphabet (this array must have 26 values). 
         // Shuffle the array and, after shuffling, display the last letter from the array. 
         // Have it also display the first letter of the array. 
         // If the first letter in the array is a vowel, have it display a message.
-        ArrayList<Character> letters26 = new ArrayList<Character>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', "k", 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
-        ArrayList<Character> sorted = uzz.Puzzle03(nameList);
-        System.out.println(sorted);
+        ArrayList<Character> sorted = uzz.PuzzeleIII();
+        System.out.println("Shuffled Alphabet: " + sorted);
 
 
         // PUZZLE 04
-        // ● Generate and return an array with 10 random numbers between 55-100.
-        ArrayList<Integer> random10 = uzz.Puzzele04();
-        System.out.println(random10);
+        System.out.println("PUZZLE 04");
+        //  Generate and return an array with 10 random numbers between 55-100.
+        ArrayList<Integer> random10 = uzz.PuzzeleIV();
+        System.out.println("An array of 10 Random Numbers between 55-100: " + random10);
 
 
         // PUZZLE 05
-        // ● Generate and return an array with 10 random numbers between 55-100 and 
+        System.out.println("PUZZLE 05");
+        //  Generate and return an array with 10 random numbers between 55-100 and 
         // have it be sorted (showing the smallest number in the beginning). 
         // Display all the numbers in the array. Next, display the minimum value in the array 
         // as well as the maximum value.
-        ArrayList<Object> random10of55To100 = uzz.Puzzele05();
-        System.out.println(random10of55To100);
+        ArrayList<Integer> random10of55To100 = uzz.PuzzeleV();
+        System.out.println("An array of 10 Random Numbers between 55-100 with the Max and Min Values at the end: " + random10of55To100);
 
 
         // PUZZLE 06
-        // ● Create a random string that is 5 characters long.
-        String randomString = uzz.Puzzle06();
-        System.out.println(randomString);
+        System.out.println("PUZZLE 06");
+        //  Create a random string that is 5 characters long.
+        String randomString = uzz.PuzzeleVI();
+        System.out.println("A random String 5 characters long: " + randomString);
 
 
         // PUZZLE 07
-        // ● Generate an array with 10 random strings that are each 5 characters long
-        ArrayList<String> randomStringArray = uzz.Puzzle06();
-        System.out.println(randomStringArray);
+        System.out.println("PUZZLE 07");
+        //  Generate an array with 10 random strings that are each 5 characters long
+        ArrayList<String> randomStringArray = uzz.PuzzeleVII();
+        System.out.println("An array with 10 random strings that are 5 characters long: " + randomStringArray);
     }
 }
