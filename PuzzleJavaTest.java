078157b75be4e280499e98926008dd48dac474df
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-30 18:19:42 -0800

    Layout of Tasks

--- PuzzleJavaTest.java
+++ PuzzleJavaTest.java
@@ -1,3 +1,35 @@
 public class PuzzleJavaTest {
+    public static void main(String[] args) {
     
+        // ● Create an array with the following values: 3,5,1,2,7,9,8,13,25,32. 
+        // Print the sum of all numbers in the array. Also have the function return an array that only 
+        // includes numbers that are greater than 10 (e.g. when you pass the array above, 
+        // it should return an array with the values of 13,25,32)
+
+
+        // ● Create an array with the following values: Nancy, Jinichi, Fujibayashi, Momochi, Ishikawa. 
+        // Shuffle the array and print the name of each person. Have the method also
+        //  return an array with names that are longer than 5 characters.
+
+
+        // ● Create an array that contains all 26 letters of the alphabet (this array must have 26 values). 
+        // Shuffle the array and, after shuffling, display the last letter from the array. 
+        // Have it also display the first letter of the array. 
+        // If the first letter in the array is a vowel, have it display a message.
+
+
+        // ● Generate and return an array with 10 random numbers between 55-100.
+
+
+        // ● Generate and return an array with 10 random numbers between 55-100 and 
+        // have it be sorted (showing the smallest number in the beginning). 
+        // Display all the numbers in the array. Next, display the minimum value in the array 
+        // as well as the maximum value.
+
+
+        // ● Create a random string that is 5 characters long.
+
+
+        // ● Generate an array with 10 random strings that are each 5 characters long
+    }
 }
